a498cd51be035dcba16276c40ce39d71fa3ddbe2
Ivan Anisimov <vanechka.anisimov.02@gmail.com>
2022-11-23 20:53:11 +0300

    base app

---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
+
+class AppController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+    }
+}
